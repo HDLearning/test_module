0da4e27a6e367c32941410a8a0da4b38905fce0f
Hamza <mlearning51@gmail.com>
2025-03-04 15:45:56 +0500

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Test Panda',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Generates an XLSX file on module installation',
+    'depends': ['base'],
+    'installable': True,
+    'auto_install': False,
+    'post_init_hook': 'post_init_hook',
+}
